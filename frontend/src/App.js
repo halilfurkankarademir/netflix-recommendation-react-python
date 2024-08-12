c02dd9c041d40485eb2bf03e92e4d264338907a4
Halil Furkan <hfurkankarademir@gmail.com>
2024-08-12 20:18:00 +0300

    upt

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MovieRecommend from './MovieRecommend';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MovieRecommend></MovieRecommend>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Navbar.jsx
@@ -0,0 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import './Navbar.css'
+
+function BasicExample() {
+  return (
+    <Navbar expand="lg" className="navbar">
+      <Container>
+        <Navbar.Brand href="#home" className='p-2' style={{fontWeight:'500'}}>Recommend AI</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto icons">
+            <Nav.Link href="https://www.linkedin.com/in/halilfurkankarademir/"><i className="bi bi-linkedin"></i></Nav.Link>
+            <Nav.Link href="https://github.com/halilfurkankarademir"><i className="bi bi-github"></i></Nav.Link>
+            <Nav.Link href="https://www.instagram.com/halilfurkankarademir/"><i className="bi bi-instagram"></i></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
